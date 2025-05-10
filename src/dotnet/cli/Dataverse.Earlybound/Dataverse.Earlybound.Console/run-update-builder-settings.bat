7be060becba3e1d96866c932b22f775549e21da1
dotnetprog <fdnadeau@techsolutionsfdn.com>
2025-05-10 16:54:15 -0400

    fux

--- src/dotnet/cli/Dataverse.Earlybound/Dataverse.Earlybound.Console/run-update-builder-settings.bat
+++ src/dotnet/cli/Dataverse.Earlybound/Dataverse.Earlybound.Console/run-update-builder-settings.bat
@@ -8,7 +8,7 @@ dotnet run --project . -- update-builder-settings --file "builderSettings.json" 
 REM Check if the previous command succeeded
 if %errorlevel%==0 (
     REM Run the pac modelbuilder command if dotnet run succeeds
-    pac modelbuilder build --outdirectory .\Entities\ --settingsTemplateFile builderSettings.json --environment "https://pcflab.crm3.dynamics.com/"
+    pac modelbuilder build --outdirectory .\EarlyboundOutput\ --settingsTemplateFile builderSettings.json --environment "https://yourEnvironment.crm3.dynamics.com/"
 )
 
 REM Pause to keep the console open after execution
